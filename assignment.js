8073838595eb0a392c7228fc2936ff721061869d
naeem676 <ali.naeem939@gmail.com>
2020-07-17 00:45:27 +0600

    first ever commit

---
+++ assignment.js
@@ -0,0 +1,21 @@
+////////start feetToMile function////////////
+
+
+function feetToMile(feet){
+          var mile = feet / 5280;
+          return mile;
+
+}
+var myMile = feetToMile();
+console.log(myMile);
+
+
+/////End feetToMile function////////////////////
+
+
+/////start woodCalculator function /////////////
+
+
+
+
+///End woodCalculator function///////////////
