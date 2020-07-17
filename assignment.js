a35ea5c9ed35ba58ae3fa90e724e61de3dae75aa
naeem676 <ali.naeem939@gmail.com>
2020-07-17 11:58:52 +0600

    woodCalculator added

--- assignment.js
+++ assignment.js
@@ -16,14 +16,21 @@ console.log(myMile);
 /////start woodCalculator function /////////////
 
 
+function woodCalculator(chair, table, bed){
+    const result = (chair*14) + (table*30) + (bed*45);
+    return result;
 
+}
+const cubicFeet = woodCalculator(4, 6, 8);
+
+console.log(cubicFeet);
 
 ///End woodCalculator function///////////////
 
 
 /////start tinyfriend function///////////
 
-var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42]
+const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42]
 
 function tinyFriend(arr){
           var min = Math.min.apply(Math, arr);
