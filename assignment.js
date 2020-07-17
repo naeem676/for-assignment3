4c590154c3e3d206fc776532235b38d90023c4a4
naeem676 <ali.naeem939@gmail.com>
2020-07-17 09:48:40 +0600

    tinyFriend function added

--- assignment.js
+++ assignment.js
@@ -2,11 +2,11 @@
 
 
 function feetToMile(feet){
-          var mile = feet / 5280;
+          const mile = feet / 5280;
           return mile;
 
 }
-var myMile = feetToMile();
+const myMile = feetToMile();
 console.log(myMile);
 
 
@@ -19,3 +19,19 @@ console.log(myMile);
 
 
 ///End woodCalculator function///////////////
+
+
+/////start tinyfriend function///////////
+
+var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42]
+
+function tinyFriend(arr){
+          var min = Math.min.apply(Math, arr);
+    return min;
+}
+
+
+
+console.log(tinyFriend(arr));
+
+//////End tiny friend function//////
